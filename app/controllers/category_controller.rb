fa71633ddc5104d26d83fd60ddd5a3ead3446765
atrapp <andrea.trapp@gmx.net>
2015-05-16 17:02:37 -0400

    added Category controller, model, migration and many-to-many relationship between Product and Category

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,30 @@
+class CategoryController < ApplicationController
+
+  # def index
+  # end
+
+  # def new
+  # end
+
+  # def create
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def show
+  # end
+
+  # def destroy
+  # end
+
+  # private
+
+  # def category_params
+  #   params.require(:category).permit(...)
+  # end
+
+end
