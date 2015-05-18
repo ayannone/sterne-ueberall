13a284eb1e8a9384b7758844b68d5007782a29e1
atrapp <andrea.trapp@gmx.net>
2015-05-18 01:30:52 -0400

    changed social icons, added sticky footer, some formatting

---
+++ app/assets/javascripts/sterne_ueberall.js
@@ -0,0 +1,8 @@
+$(function(){
+  alert("Willkommen");
+  $("#product1").on(mouseover,function(){}
+  // $("#product1").hover(function(){
+    // $('img', this).css('opacity','0.3');
+    $(".products").css("background","red");
+  });
+}
