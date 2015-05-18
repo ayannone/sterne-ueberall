a1f4131f65e2318401abf276428d1712c21c147e
atrapp <andrea.trapp@gmx.net>
2015-05-18 01:34:30 -0400

    commented out code in sterne_ueberall.js

--- app/assets/javascripts/sterne_ueberall.js
+++ app/assets/javascripts/sterne_ueberall.js
@@ -1,8 +1,8 @@
-$(function(){
-  alert("Willkommen");
-  $("#product1").on(mouseover,function(){}
-  // $("#product1").hover(function(){
-    // $('img', this).css('opacity','0.3');
-    $(".products").css("background","red");
-  });
-}
+// $(function(){
+//   alert("Willkommen");
+//   $("#product1").on(mouseover,function(){}
+//   // $("#product1").hover(function(){
+//     // $('img', this).css('opacity','0.3');
+//     $(".products").css("background","red");
+//   });
+// }
