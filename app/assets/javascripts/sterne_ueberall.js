0655cf299a6c791e1a86925863abc115f136fbeb
atrapp <andrea.trapp@gmx.net>
2015-05-18 14:04:42 -0400

    products formatting with hover effect

--- app/assets/javascripts/sterne_ueberall.js
+++ app/assets/javascripts/sterne_ueberall.js
@@ -1,8 +1,25 @@
-// $(function(){
-//   alert("Willkommen");
-//   $("#product1").on(mouseover,function(){}
-//   // $("#product1").hover(function(){
-//     // $('img', this).css('opacity','0.3');
-//     $(".products").css("background","red");
-//   });
-// }
+$(function(){
+
+  // $("#product1").on("mouseover",function(){
+  //   $('img', this).css('opacity','0.5');
+  // });
+  // $("#product1").on("mouseout",function(){
+  //   $('img', this).css('opacity','1');
+  // });
+
+  var productGroups = ['#product1','#product2','#product3','#product4'];
+  productGroups.forEach(function(productGroup) {
+    $(productGroup).hover(
+    function(){
+      $(productGroup).css('background-color','black');
+      $('img', this).css('opacity','0.4');
+      $('h3', this).css('color','black');
+    },
+    function(){
+      $(productGroup).css('background-color','transparent');
+      $('img', this).css('opacity','1');
+      $('h3', this).css('color','white');
+    });
+  });
+
+})
